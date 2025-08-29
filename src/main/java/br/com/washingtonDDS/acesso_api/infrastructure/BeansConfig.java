76c6a6585568cec8cd5d6975f86e6829730a6107
WashingtonDDS <washington.DDS@hotmail.com>
2025-08-28 23:27:55 -0300

    chore: cria BeansConfig para configuração do bean UserUseCase

--- src/main/java/br/com/washingtonDDS/acesso_api/infrastructure/BeansConfig.java
+++ src/main/java/br/com/washingtonDDS/acesso_api/infrastructure/BeansConfig.java
@@ -1,14 +1,17 @@
 package br.com.washingtonDDS.acesso_api.infrastructure;
 
+import br.com.washingtonDDS.acesso_api.adapter.input.mapper.UserMapper;
 import br.com.washingtonDDS.acesso_api.core.usecase.UserUseCase;
+import br.com.washingtonDDS.acesso_api.port.output.UserOutputPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeansConfig {
 
+
     @Bean
-    public UserUseCase userUseCaseimpl(){
-        return new UserUseCase();
+    public UserUseCase userUseCaseimpl(UserOutputPort userOutputPort){
+        return new UserUseCase(userOutputPort);
     }
 }
