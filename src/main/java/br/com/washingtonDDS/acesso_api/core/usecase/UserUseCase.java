bbe0c780e10cdcbb2900158d2733c8257646040d
WashingtonDDS <washington.DDS@hotmail.com>
2025-08-28 23:27:37 -0300

    feat: adiciona construtor no UserUseCase para injeção de dependência

--- src/main/java/br/com/washingtonDDS/acesso_api/core/usecase/UserUseCase.java
+++ src/main/java/br/com/washingtonDDS/acesso_api/core/usecase/UserUseCase.java
@@ -2,10 +2,18 @@ package br.com.washingtonDDS.acesso_api.core.usecase;
 
 import br.com.washingtonDDS.acesso_api.core.domain.model.User;
 import br.com.washingtonDDS.acesso_api.port.input.UserInputPort;
+import br.com.washingtonDDS.acesso_api.port.output.UserOutputPort;
 
 public class UserUseCase implements UserInputPort {
+
+    private final UserOutputPort userOutputPort;
+
+    public UserUseCase(UserOutputPort userOutputPort) {
+        this.userOutputPort = userOutputPort;
+    }
+
     @Override
     public User create(User user) {
-        return user;
+        return userOutputPort.save(user);
     }
 }
