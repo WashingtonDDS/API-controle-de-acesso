d3411ab80378cda8a8d36dfe1abf847c14432113
WashingtonDDS <washington.DDS@hotmail.com>
2025-09-05 20:37:22 -0300

    feat(VisitorInputPort): criar interface para entrada de dados de visitantes

---
+++ src/main/java/br/com/washingtonDDS/acesso_api/port/input/VisitorInputPort.java
@@ -0,0 +1,7 @@
+package br.com.washingtonDDS.acesso_api.port.input;
+
+import br.com.washingtonDDS.acesso_api.core.domain.model.Visitor;
+
+public interface VisitorInputPort {
+    Visitor create(Visitor visitor);
+}
